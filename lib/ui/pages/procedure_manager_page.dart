985c770bf58d5f3f01ac3d300614af1cd2b604db
zengyanren <zengyanren@gdmcmc.cn>
2020-05-16 22:05:01 +0800

    feat 增加接收弹出框

--- lib/ui/pages/procedure_manager_page.dart
+++ lib/ui/pages/procedure_manager_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:manufacture/beans/base_bean.dart';
 import 'package:manufacture/beans/mop.dart';
 import 'package:manufacture/beans/procedure.dart';
@@ -34,22 +35,26 @@ class _ProcedureManagerState extends State<ProcedureManager> {
     super.initState();
   }
 
-  Widget _actionButton(String actionName, Icon icon,
-      VoidCallback callback) {
+  Widget _actionButton(String actionName, Icon icon, VoidCallback callback) {
     return PopupMenuItem(
       child: ListTile(
         leading: icon != null ? Icon(Icons.forward) : icon,
         title: Text(actionName),
-        onTap: (){
-          callback();
+        onTap: () {
           Navigator.pop(context);
+          callback();
+
         },
       ),
     );
   }
 
-  ListTile _procedureNum(String name, int cnt){
-    return ListTile(dense:true,leading: Text(name),title:Text("$cnt"),);
+  ListTile _procedureNum(String name, int cnt) {
+    return ListTile(
+      dense: true,
+      leading: Text(name),
+      title: Text("$cnt"),
+    );
   }
 
   @override
@@ -69,14 +74,14 @@ class _ProcedureManagerState extends State<ProcedureManager> {
                 print(list);
                 return RailWay(
                   physics: NeverScrollableScrollPhysics(),
-
                   stations: list.map((node) {
                     print(node);
                     Procedure _procedure = node as Procedure;
                     return Station(
-                        onTap: (){
+                        onTap: () {
                           print("--------onTap-------");
-                          Navigator.push(context, MaterialPageRoute(builder: (context){
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
                             //return ProcedureHomePage(procedure: _procedure,);
                             return TaskMangePage(procedure: _procedure);
                           }));
@@ -90,18 +95,55 @@ class _ProcedureManagerState extends State<ProcedureManager> {
                           ],
                         ),
                         actionBuilder: (context, int) {
+                          TextEditingController _countController = new TextEditingController();
                           return PopupMenuButton(
                             icon: Icon(Icons.more_vert),
                             itemBuilder: (context) => <PopupMenuItem>[
-                              _actionButton("发送", Icon(Icons.forward), (){
-                                  print("发送:"+_procedure.name);
+                              _actionButton("发送", Icon(Icons.forward), () {
+                                print("发送:" + _procedure.name);
                               }),
-                              _actionButton("接收", Icon(Icons.forward), (){
-
+                              _actionButton("接收", Icon(Icons.forward), () {
+                                print("接收:" + _procedure.name);
+                                showDialog(
+                                    builder: (context) => new AlertDialog(
+                                          title: new Text('输入接收数量'),
+                                          content: Builder(
+                                            builder: (context){
+                                              return TextField (
+                                                controller: _countController,
+                                                keyboardType: TextInputType.number,
+                                                inputFormatters: [
+                                                  WhitelistingTextInputFormatter.digitsOnly
+                                                ],
+                                                decoration: InputDecoration(
+                                                  filled: true,
+                                                  icon: Icon(
+                                                    Icons.account_balance,
+                                                    color: Theme.of(context).accentColor,
+                                                  ),
+                                                  labelText: '数量',
+                                                  //hintText: '编号',
+                                                  //border: OutlineInputBorder(borderRadius: const BorderRadius.all(Radius.circular(20.0)),)
+                                                ),
+                                              );
+                                            },
+                                          ),
+                                          actions: <Widget>[
+                                            new FlatButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: new Text('确定')),
+                                            new FlatButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: new Text('取消'))
+                                          ],
+                                        ),
+                                    context: context);
                               }),
-                              _actionButton("派工", Icon(Icons.forward), (){
-
-                              }),
+                              _actionButton("派工", Icon(Icons.forward), () {}),
                             ],
                           );
                         });
